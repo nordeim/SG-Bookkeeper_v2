313965710dc99fd0aa2734b689e6dc7b6bfa60cc
nordeim <nordeimkuahsziyp@outlook.com>
2025-09-13 13:00:38 +0800

    Update run_sg_bookkeeper.bat

--- run_sg_bookkeeper.bat
+++ run_sg_bookkeeper.bat
@@ -13,5 +13,12 @@ set PGHOST=localhost
 set PGPORT=5432
 set PGDATABASE=sg_bookkeeper
 
+REM Debug: Show environment variable values
+echo PGUSER=%PGUSER%
+echo PGPASSWORD=%PGPASSWORD%
+echo PGHOST=%PGHOST%
+echo PGPORT=%PGPORT%
+echo PGDATABASE=%PGDATABASE%
+
 REM Launch the SG Bookkeeper application
 poetry run sg_bookkeeper
